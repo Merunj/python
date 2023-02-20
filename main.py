9ca2f461203b1d87c148b48f24664c35df18c80f
danya <meowsweep@gmail.com>
2023-02-20 23:25:17 +0300

    третье задание

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 app = Flask(__name__)
 
 
-@app.route('/training/<prof>')
-def proffesion(prof):
-    return render_template("index.html", prof=prof)
+@app.route('/list_prof/<url_list>')
+def proffesion(url_list):
+    return render_template("index.html", url_list=url_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
