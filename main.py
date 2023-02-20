f9c7b44dd5473ad1a8b25bc7e14870dffd4268df
danya <meowsweep@gmail.com>
2023-02-20 17:30:40 +0300

    commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return render_template("index.html")
+    return render_template("index.html", title="Загатовка")
 
 if __name__ == "__main__":
     app.run(debug=True)
